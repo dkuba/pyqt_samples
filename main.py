9693e83b01a5213aaa06acbcf4c88b139113cae8
dkuba <dkubarev@ya.ru>
2019-12-13 14:39:05 +0300

    создал скелетон PyQt приложения

---
+++ main.py
@@ -0,0 +1,14 @@
+
+import sys
+from PyQt5.QtWidgets import QApplication, QStyleFactory
+from mainWindow import *
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    QApplication.setStyle(QStyleFactory.create('Fusion'))
+    # app.setStyleSheet(str(qssStr))
+    myGUI = MainAppWin()
+    myGUI.show()
+    sys.exit(app.exec_())
